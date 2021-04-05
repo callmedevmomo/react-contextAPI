326fc43d41e5211c596c19f06f724a986f50ff5f
momo <callmedevmomo@gmail.com>
2021-04-05 21:45:09 +0900

    two different ways of consuming context by components type

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,34 @@
-import React,{ Component } from 'react';
+import React, {Component} from 'react';
+import {ThemeContext} from '../contexts/ThemeContext';
 
 class Navbar extends Component {
-    render() { 
-        return (  
-<nav>
-    <h1>Context App</h1>
-    <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-    </ul>
-</nav>
-
+    render() {
+        return (
+            <ThemeContext.Consumer>{
+                    (context) => {
+                        const {isLightTheme, light, dark} = context;
+                        const theme = isLightTheme
+                            ? light
+                            : dark;
+                        return (
+                            <nav
+                                style={{
+                                    background: theme.ui,
+                                    color: theme.syntax
+                                }}>
+                                <h1>Context App</h1>
+                                <ul>
+                                    <li>Home</li>
+                                    <li>About</li>
+                                    <li>Contact</li>
+                                </ul>
+                            </nav>
+                        )
+                    }
+                }
+            </ThemeContext.Consumer>
         );
     }
 }
- 
+
 export default Navbar;
